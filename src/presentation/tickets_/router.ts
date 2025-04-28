bd9739f224fa5d8cc454861030b6d0ef6c9b7623
Batman <diegoalejandromona12@gmail.com>
2025-04-28 08:33:47 -0500

    feat: :sparkles: Added adjustment in tickets

--- src/presentation/tickets_/router.ts
+++ src/presentation/tickets_/router.ts
@@ -17,6 +17,10 @@ export class TicketRoutes_ {
     const ticketService = new TicketService_(emailService);
     const ticketController = new TicketsController_(ticketService);
 
+    // CUIDADO
+    // no tocar estos endpoints, ni los métodos en el controller ni el service, si necesitan cambiar algo, crear otro endpoint
+    // y otro método en el controller y el service
+
     router.get("/", ticketController.getTickets); // traer todos los tickets creados en todas las sedes
     router.get("/:id", [AuthMiddlewre.validateJWT], ticketController.getTicketById); // traer información del ticket
     router.post("/", [AuthMiddlewre.validateJWT], ticketController.createTicket); // crear ticket
