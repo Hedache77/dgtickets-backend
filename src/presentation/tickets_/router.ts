8cf4b556a0587f55038b11eff2f410904d472e59
Batman <diegoalejandromona12@gmail.com>
2025-04-28 08:32:19 -0500

    feat: :sparkles: Added adjustment in tickets

--- src/presentation/tickets_/router.ts
+++ src/presentation/tickets_/router.ts
@@ -3,6 +3,7 @@ import { TicketsController_ } from "./controller";
 import { TicketService_ } from "../services/ticket_.service";
 import { envs } from "../../config";
 import { EmailService } from "../services";
+import { AuthMiddlewre } from "../middlewares/auth.middleware";
 
 export class TicketRoutes_ {
   static get routes(): Router {
@@ -17,12 +18,12 @@ export class TicketRoutes_ {
     const ticketController = new TicketsController_(ticketService);
 
     router.get("/", ticketController.getTickets); // traer todos los tickets creados en todas las sedes
-    router.get("/:id", ticketController.getTicketById); // traer información del ticket
-    router.post("/", ticketController.createTicket); // crear ticket
-    router.put("/:id", ticketController.updateTicket); // id de ticket para actualizar
+    router.get("/:id", [AuthMiddlewre.validateJWT], ticketController.getTicketById); // traer información del ticket
+    router.post("/", [AuthMiddlewre.validateJWT], ticketController.createTicket); // crear ticket
+    router.put("/:id", [AuthMiddlewre.validateJWT], ticketController.updateTicket); // id de ticket para actualizar
     router.get("/row/:id", ticketController.getTicketByRow); // id de la sede para obtener la cola sin prioridad
     router.get("/priority/:id", ticketController.getTicketPriority); // id de la sede para obtener la cola con prioridad
-    router.get("/position/:id", ticketController.calculatePositionTime); // id de ticket para saber la posición y el tiempo de atención
+    router.get("/position/:id", [AuthMiddlewre.validateJWT], ticketController.calculatePositionTime); // id de ticket para saber la posición y el tiempo de atención
 
     return router;
   }
